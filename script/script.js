293ead1b590d54f806b19c28e7862937cc0570a5
Rajamanickam <rajamanickam842001@gmail.com>
2023-02-16 12:49:45 +0530

    first javascript commit

---
+++ script/script.js
@@ -0,0 +1,100 @@
+// let country = "India";
+// let continent = "Asia";              
+// let population = "200 Billian";
+// console.log(country)
+// console.log(continent)
+// console.log(population)
+
+// Data Types -- let, const and var
+  
+// const language = "Tamil";
+// let country = "India";
+// let continent = "Asia";
+// var population = 600;
+// let finLand = 200;
+// let averagePopulation = 3300;
+// let isRaining = true;
+// const description = country + " is in " + continent + " , and its " + population + " million people speak " + language;
+
+// console.log(typeof language);
+// console.log(typeof country);
+// console.log(typeof state);
+// console.log(typeof isRaining);
+// console.log(typeof population);
+
+// console.log(population/2);
+// console.log(population + 1);        //Basic Operators
+// console.log(population > finLand);
+// console.log(averagePopulation < population);
+// console.log(description);
+
+
+// coding challenge in BMI calculation
+
+// let markMass = 78;
+// let johnMass = 95;
+
+// let markHeight =1.69;               
+// let johnHeight =1.88;
+
+// let markBMI = markMass / markHeight **2;
+// let johnBMI = johnMass / (johnHeight*johnHeight);
+
+// let higherBMI = markBMI > johnBMI;
+
+// console.log("Mark Weight :",markMass);
+// console.log("Mark Height :",markHeight);
+// console.log("John Weight :",johnMass);
+// console.log("John Height :",johnHeight);
+
+// console.log("MarkBMI :",markBMI);
+// console.log("JohnBMI :",johnBMI);
+// console.log("Mark is Higher BMI compared to John :",higherBMI);
+
+
+// //String and Template Literals
+// const descriptionOne = `"${country} is in ${continent}, and its ${population} people speak ${language}"`
+
+// console.log(descriptionOne);
+
+
+// Taking Decisions: if / else Statements
+
+// let country = "India";
+// let continent = "Asia";
+// var population = 40;
+// let averagePopulation = 33;
+// let average = averagePopulation - population;
+
+// if(population > averagePopulation){
+//   console.log(`${continent} population is above average`);
+// }else{
+//   console.log(`${continent} population is ${average} million below average`);
+// }
+
+// coding challenge in BMI checking
+
+// let markMass = 68;
+// let johnMass = 75;
+
+// let markHeight =1.69;               
+// let johnHeight =1.88;
+
+// let markBMI = markMass / markHeight **2;
+// let johnBMI = johnMass / (johnHeight*johnHeight);
+
+// console.log("MarkBMI :",markBMI);
+// console.log("JohnBMI :",johnBMI);
+
+// if(markBMI > johnBMI){
+//   console.log(`Mark's BMI is higher than john's!`);
+// }else{
+//   console.log(`John's is higher than Mark's!`);
+// }
+
+// //challenge to be explained in BMI values and using toFixed value
+// if(markBMI > johnBMI){
+//   console.log(`Mark's BMI ${markBMI.toFixed(2)} is higher than john's ${johnBMI.toFixed(2)}!`);
+// }else{
+//   console.log(`John's ${johnBMI.toFixed(2)} is higher than Mark's ${markBMI.toFixed(2)}!`);
+// }
